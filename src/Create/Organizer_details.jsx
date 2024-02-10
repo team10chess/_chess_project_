d1ef10793381240e345a2a0c2ff1a0c7cfb2d6f7
Bhuvanchandra M <bhuvanchandra668@gmail.com>
2024-02-10 07:58:51 +0530

    Changes

--- src/Create/Organizer_details.jsx
+++ src/Create/Organizer_details.jsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { useState } from 'react';
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
@@ -9,70 +10,101 @@ import FormControl from "@mui/material/FormControl";
 import FormLabel from "@mui/material/FormLabel";
 
 export default function AddressForm() {
+
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    gender: "",
+    orgid: "",
+  });
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const jsonData = JSON.stringify(formData);
+    console.log(jsonData);
+  };
+  
+  const handleChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
   return (
     <React.Fragment>
       <Typography variant="h6" gutterBottom>
         Organizer details
       </Typography>
-      <Grid container spacing={3}>
-        <Grid item xs={12} sm={25}>
-          <TextField
-            required
-            id="firstName"
-            name="firstName"
-            label="First name"
-            fullWidth
-            autoComplete="given-name"
-            variant="standard"
-          />
+      <form onSubmit={handleSubmit}>
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={25}>
+            <TextField
+              required
+              id="firstName"
+              name="firstName"
+              label="First name"
+              fullWidth
+              autoComplete="given-name"
+              variant="standard"
+              onChange={handleChange}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              required
+              id="lastName"
+              name="lastName"
+              label="Last name"
+              fullWidth
+              autoComplete="family-name"
+              variant="standard"
+              onChange={handleChange}
+            />
+          </Grid>
+          <Grid item xs={12} sm={10}>
+            <FormControl>
+              <FormLabel id="demo-row-radio-buttons-group-label">
+                Gender
+              </FormLabel>
+              <RadioGroup
+                row
+                aria-labelledby="demo-row-radio-buttons-group-label"
+                name="row-radio-buttons-group"
+                onChange={handleChange}
+              >
+                <FormControlLabel
+                  value="female"
+                  control={<Radio />}
+                  label="Female"
+                />
+                <FormControlLabel
+                  value="male"
+                  control={<Radio />}
+                  label="Male"
+                />
+                <FormControlLabel
+                  value="other"
+                  control={<Radio />}
+                  label="Other"
+                />
+              </RadioGroup>
+            </FormControl>
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              required
+              id="orgid"
+              name="orgid"
+              label="Organizer ID"
+              fullWidth
+              autoComplete="orgid"
+              variant="standard"
+              onChange={handleChange}
+            />
+          </Grid>
         </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            required
-            id="lastName"
-            name="lastName"
-            label="Last name"
-            fullWidth
-            autoComplete="family-name"
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12} sm={10}>
-          <FormControl>
-            <FormLabel id="demo-row-radio-buttons-group-label">
-              Gender
-            </FormLabel>
-            <RadioGroup
-              row
-              aria-labelledby="demo-row-radio-buttons-group-label"
-              name="row-radio-buttons-group"
-            >
-              <FormControlLabel
-                value="female"
-                control={<Radio />}
-                label="Female"
-              />
-              <FormControlLabel value="male" control={<Radio />} label="Male" />
-              <FormControlLabel
-                value="other"
-                control={<Radio />}
-                label="Other"
-              />
-            </RadioGroup>
-          </FormControl>
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            required
-            id="orgid"
-            name="orgid"
-            label="Organizer ID"
-            fullWidth
-            autoComplete="orgid"
-            variant="standard"
-          />
-        </Grid >
-      </Grid>
+      </form>
     </React.Fragment>
   );
 }
